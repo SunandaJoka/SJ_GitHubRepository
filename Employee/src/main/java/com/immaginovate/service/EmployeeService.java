ea3b9fb9f92e83bcf4bb7732886626fe71915aa7
SunandaJoka <sunandajoka13@gmail.com>
2023-05-09 19:13:22 +0530

    Final Code Commit

--- Employee/src/main/java/com/immaginovate/service/EmployeeService.java
+++ Employee/src/main/java/com/immaginovate/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.immaginovate.service;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,15 +29,46 @@ public class EmployeeService {
 		Calendar cal=Calendar.getInstance();
 		cal.setTime(dt);
 		int month=cal.get(Calendar.MONTH);
+		int salariedMonths;
 		if(Integer.valueOf(2)>month) {
-			
+			salariedMonths= (11-month) +3;
+		}else {
+			salariedMonths=month-2;
 		}
+		int salariedDays=0;
+		double salaryPerDay=(emp.getSalary()/30);
+		int date= cal.get(Calendar.DAY_OF_MONTH);
+		Calendar tempCal= new GregorianCalendar(Calendar.YEAR, month, 1);
+		int daysInMonth= tempCal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		salariedDays= daysInMonth-date;
+		
+		double totalSal=(salariedMonths*emp.getSalary())+(salariedDays*(emp.getSalary()/salaryPerDay));
+				
+		model.setId(emp.getId());
+		model.setFirstName(emp.getFirstName());
+		model.setLastName(emp.getLastName());
+		model.setYrlySalary(totalSal);
+		if(totalSal>2500000) {
+			model.setCessAmnt((totalSal-2500000)*0.02);
+		}else {
+			model.setCessAmnt(0.0d);
+		}
+		if(totalSal<250000 || totalSal==250000) {
+			model.setTaxAmnt(0.0d);
+		}else if(totalSal<500000 || totalSal==500000){
+			model.setTaxAmnt((totalSal-250000)*0.05);
+		}else if(totalSal<1000000 || totalSal==1000000) {
+			model.setTaxAmnt((250000*0.05)+((totalSal-500000)*0.1));
+		}else if(totalSal>1000000) {
+			model.setTaxAmnt((250000*0.05)+(500000*0.1)+((totalSal-1000000)*0.2));
+		}		
 		
 		}
 		
 		
 		return model;
-		
-		
+				
 	}
+
+	
 }
