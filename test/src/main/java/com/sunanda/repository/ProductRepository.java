d8072b9a94d0f18d236a9fa3cbb748562fbf67c5
SunandaJoka <sunandajoka13@gmail.com>
2023-05-09 13:35:38 +0530

    First Test Code

---
+++ test/src/main/java/com/sunanda/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sunanda.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sunanda.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+	Product findByName(String name);
+
+}
---
+++ test/src/main/java/com/sunanda/test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.sunanda.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
